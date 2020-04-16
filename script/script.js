1b4b838696cea5ad3f9c8091e3ea1f3a605e0b8c
Evgeniy Gulak <gulllak@yandex.ru>
2020-04-16 12:24:30 +0300

    version 1.0

---
+++ script/script.js
@@ -0,0 +1,31 @@
+const popup = document.querySelector('.popup');
+const popupOpened = popup.querySelector('.popup_opened');
+const closeButton = popup.querySelector('.popup__close');
+const editButton = document.querySelector('.profile__edit-button');
+const saveButton = document.querySelector('.popup__btn-save');
+
+let popupUsername = document.querySelector('.popup__username');
+let popupSignature = document.querySelector('.popup__signature');
+let profileName = document.querySelector('.profile__name');
+let profileSignature = document.querySelector('.profile__signature');
+
+function editForm() {
+  if (popup.classList.contains('popup_opened') === false) {
+    popup.classList.add('popup_opened');
+    popupUsername.value = profileName.textContent;
+    popupSignature.value = profileSignature.textContent;
+  } else {
+    popup.classList.remove('popup_opened');
+  }
+}
+
+function saveForm (event) {
+  event.preventDefault();
+  profileName.textContent = popupUsername.value;
+  profileSignature.textContent = popupSignature.value;
+  editForm();
+}
+
+editButton.addEventListener("click", editForm);
+closeButton.addEventListener("click", editForm);
+saveButton.addEventListener('click', saveForm);
